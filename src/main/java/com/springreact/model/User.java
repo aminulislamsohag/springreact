2a140cfc967d0d0fc97e61475fe3bc228572159b
aminulislamsohag <38567911+aminulislamsohag@users.noreply.github.com>
2024-05-05 17:28:20 +0600

    addUser
    show and add user frontend page create.

--- src/main/java/com/springreact/model/User.java
+++ src/main/java/com/springreact/model/User.java
@@ -3,13 +3,16 @@ package com.springreact.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name= "user")
 public class User {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
     private Long id;
     private String username;
     private String name;
